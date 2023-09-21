864d23370a598a18145af9f263caa9b77df1f71e
bjofrec <brandonunic@gmail.com>
2023-09-20 23:12:52 -0300

    Evento Onclick

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function logout(elemento) {
+    const elementoLogout = document.querySelector('#btn');
+    btn.innerText = "Logout";
+}
+
+function removerAdd(elemento) {
+    const elementoRemover = document.querySelector('.add-def');
+    elementoRemover.remove();
+}
+
+function alertaLike(elemento) {
+    alert("ninja was liked");
+}
